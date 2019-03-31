2e807da33c51c92ffc7afa5ae48dc9acf052b2bb
Sara Scaini <sa.scaini@gmail.com>
2019-03-31 11:46:34 +0100

    test

--- projects.js
+++ projects.js
@@ -1,16 +1,21 @@
 
-const project = document.getElementById("project");
-const image1  = document.getElementById("image1");
-const image2  = document.getElementById("image2")
-project.addEventListener("mouseenter", (event) => {
-  event.preventDefault();
-  image1.classList.add("d-none");
-  image2.classList.remove("d-none")
- });
+// test show image on hover
 
 
-project.addEventListener("mouseleave", (event) => {
-  event.preventDefault();
-  image2.classList.add("d-none");
-  image1.classList.remove("d-none");
- });
+// const project = document.getElementById("project");
+// const image1  = document.getElementById("image1");
+// const image2  = document.getElementById("image2")
+// project.addEventListener("mouseenter", (event) => {
+//   event.preventDefault();
+//   image1.classList.add("d-none");
+//   image2.classList.remove("d-none")
+//  });
+
+
+// project.addEventListener("mouseleave", (event) => {
+//   event.preventDefault();
+//   image2.classList.add("d-none");
+//   image1.classList.remove("d-none");
+//  });
+
+
